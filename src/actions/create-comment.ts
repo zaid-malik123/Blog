0e388dc35fcea0d67d69b20c5221ae501693e39c
Zaid Malik <mzaidmalik25@gmail.com>
2026-04-15 08:05:05 +0530

    feat: refactor createComment function for improved error handling and code clarity

--- src/actions/create-comment.ts
+++ src/actions/create-comment.ts
@@ -1,44 +1,82 @@
 import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
-import {z} from "zod";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
 
 const createCommentSchema = z.object({
-    body: z.string().min(1)
-})
+  body: z.string().min(1),
+});
 
 type createFormState = {
-    errors: {
-        body?: string[],
-        formErrors?: string[]
-    }
-}
+  errors: {
+    body?: string[];
+    formErrors?: string[];
+  };
+};
 
-export const createComment = async (articleId: string, prevState: createFormState,formData: FormData):Promise<createFormState> => {
-    const result = createCommentSchema.safeParse({
-        body:formData.get('body') as string
-    });
-    if(!result.success){
-        return {
-            errors: {
-                body: result.error.flatten().fieldErrors.body
-            }
-        }
-    }
+export const createComment = async (
+  articleId: string,
+  prevState: createFormState,
+  formData: FormData,
+): Promise<createFormState> => {
+  const result = createCommentSchema.safeParse({
+    body: formData.get("body") as string,
+  });
+  if (!result.success) {
+    return {
+      errors: {
+        body: result.error.flatten().fieldErrors.body,
+      },
+    };
+  }
 
-    const {userId} = await auth();
+  const { userId } = await auth();
 
-    if(!userId) {
-        return {
-            errors: {
-                formErrors: ["User Id not find"]
-            }
-        }
-    }
+  if (!userId) {
+    return {
+      errors: {
+        formErrors: ["User Id not find"],
+      },
+    };
+  }
 
-    const existingUser = await prisma.article.findFirst({
-        where: {
-            
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+  if (!existingUser) {
+    return {
+      errors: {
+        formErrors: ["User not found. Please register before adding comment."],
+      },
+    };
+  }
+
+  try {
+    await prisma.comment.create({
+        data: {
+            body: result.data.body,
+            authorId: existingUser.id,
+            articleId: articleId
         }
     })
-    
-}
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          formErrors: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          formErrors: ["Some internal server error while creating comment"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(`/article/${articleId}`);
+  return { errors: {} };
+};
